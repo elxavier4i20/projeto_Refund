a5476697bec4900bc6c315d3031d52c589db8fb6
Elson Xavier <elxaves83@gmail.com>
2025-03-13 21:28:23 -0300

    Calculando o valor total

--- refund/script.js
+++ refund/script.js
@@ -7,6 +7,7 @@ const category = document.getElementById("category");
 // Seleciona os elementos da lista:
 const expenseList = document.querySelector("ul");
 const expensesQuantity = document.querySelector("aside header p span");
+const expensesTotal = document.querySelector("aside header h2");
 
 // Capturando o evento de input para formatar o valor:
 amount.oninput = () => {
@@ -119,11 +120,29 @@ function updateTotals() {
     let total = 0;
 
     // Percorre cada item (li) da lista (ul):
-    for (let item = 0; item < item.length; item++) {
+    for (let item = 0; item < items.length; item++) {
       const itemAmount = items[item].querySelector(".expense-amount");
 
-      console.log(itemAmount);
+      // Remover caracteres não numéricos e substitui a vírgula pelo ponto
+      let value = itemAmount.textContent
+        .replace(/[^\d]/g, "")
+        .replace(",", ".");
+
+      // Converte o valor para float:
+      value = parseFloat(value);
+
+      // Verifica se é um número válido:
+      if (isNaN(value)) {
+        return alert(
+          "Não foi possível calcular o total. O valor precisa ser um número."
+        );
+      }
+
+      // Incrementa valor total:
+      total += Number(value);
     }
+
+    expensesTotal.textContent = total;
   } catch (error) {
     console.log(error);
     alert("Não foi possível atualizar os totais");
